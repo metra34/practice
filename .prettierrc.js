5b9f8265237eca7220039e2ce9c0d0363e0d66cc
Artem Clement <metra1334@gmail.com>
2024-02-07 12:38:25 -0500

    moving from master to main

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/* jshint esversion: 6 */
+/* https://json.schemastore.org/prettierrc */
+
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'ignore',
+	printWidth: 150,
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	vueIndentScriptAndStyle: true,
+};
